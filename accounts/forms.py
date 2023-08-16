488dff9bd105e44462e9862288ec9601b791b09b
IvanKovalik <qwezx004@yandex.ru>
2023-08-16 09:33:56 +0700

    Working hard

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, BaseUserCreationForm
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = CustomUser
+#         fields = ('email', 'username',)
+
+class CustomUserCreationForm(BaseUserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('email', 'username',)
+        fields = ('email', 'username', 'password1', 'password2')
 
 
 class CustomUserChangeForm(UserChangeForm):
